ae00750aff0faccd4ff160c00ac160301ab1d807
Jueun <jueunsuh@gmail.com>
2021-06-22 14:08:20 +0900

    init project

---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  const name = undefined;
+  return (
+    <>
+      <h1>Hello :)</h1>
+      {name && <h1>Hello! {name}:)</h1>}
+    {['💚', '🍎'].map(item => (
+    <h1>{item}</h1>
+    ))}
+    </>
+  );
+}
+
+export default App;
