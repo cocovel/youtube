d9ded7a5a09aa1b026badfd4234bcaa15c544e17
Jueun <jueunsuh@gmail.com>
2021-07-16 16:20:58 +0900

    search header function

--- src/app.jsx
+++ src/app.jsx
@@ -5,6 +5,23 @@ import SearchHeader from "./components/search_header/search_header";
 
 function App() {
   const [video, setVideos] = useState([]);
+  const search = query => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyACNMTwA048c1EtPbxSuZ5li7G0kQWNx9o`,
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result =>
+        result.items.map(item => ({ ...item, id: item.id.videoId }))
+      )
+      .then(items => setVideos(items))
+      .catch(error => console.log("error", error));
+  };
 
   useEffect(() => {
     const requestOptions = {
@@ -22,7 +39,7 @@ function App() {
   }, []);
   return (
     <div className={StyleSheet.app}>
-      <SearchHeader />
+      <SearchHeader onSearch={search} />
       <VideoList key={video.id} video={video} />
     </div>
   );
